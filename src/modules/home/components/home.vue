ab642f8cf8d8a2f52bbfdf0e69f6c7cdf761b1f4
Rico <rico@logichome.me>
2017-12-05 18:04:26 +0800

    no message

--- src/modules/home/components/home.vue
+++ src/modules/home/components/home.vue
@@ -1,6 +1,21 @@
 <template>
 <div class="home">
-  home
+  <div class="header">
+    <p class="header-title word7 title2">北海市中安止泊园</p>
+    <div class="header-button-list">
+      <span class="header-button-item">
+        <img src="../static/imgs/zhuye_xuanzeshequ.png" alt="">
+      </span>
+      <span class="header-button-item">
+        <img src="../static/imgs/zhuye_saoyisao.png" alt="">
+      </span>
+    </div>
+  </div>
+  <div class="date-info">
+    <div class="date"></div>
+    <div class="air"></div>
+    <div class="weather"></div>
+  </div>
 </div>
 </template>
 <script>
@@ -12,4 +27,20 @@ export default {
 }
 </script>
 <style lang="stylus" rel="stylesheet/stylus" scoped>
+.home
+  .header
+    position relative
+    .header-title
+      line-height 0.44rem
+      padding-left 0.15rem
+    .header-button-list
+      position absolute
+      top 0.05rem
+      right 0.05rem
+      .header-button-item
+        padding 0.06rem
+        float left
+        img 
+          width 0.22rem
+          height 0.22rem
 </style>
