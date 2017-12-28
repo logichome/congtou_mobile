fbfcfd47076e2ebeca32e075ac7bf08e1196ffb1
Rico <rico@logichome.me>
2017-12-28 17:40:15 +0800

    no message

--- src/modules/community/components/community.vue
+++ src/modules/community/components/community.vue
@@ -11,13 +11,109 @@
         </span>
       </div>
     </div>
+    <div class="community-body">
+      <swipe></swipe>
+      <div class="subject-banner">
+        <div class="subject-info">
+          <p class="subject-info-title word1">旅途中，遇见最美的风景</p>
+          <p class="subject-info-count title3 word1">已有108392人参与</p>
+          <button class="more-subject title3 word7">更多话题</button>
+        </div>
+        <div class="subject-content bg3 text1 word6">
+          <p>旅行，是偷得浮生半日闲！</p>
+          <p>美照，是拍的一张永流传！</p>
+          <p>机票的附加值，晒了才能get！</p>
+          <p>旅途的风景，分享了才更美丽！</p>
+        </div>
+        <div class="subject-top-user">
+          <div class="subject-top-title">
+            <span class="title3 word7">活跃榜</span>
+            <span class="title4 word5">71</span>
+          </div>
+          <div class="subject-top-avatar">
+            <ul class="clearfix">
+              <li>
+                <div class="top-avatar-image"></div>
+              </li>
+              <li>
+                <div class="top-avatar-image"></div>
+              </li>
+              <li>
+                <div class="top-avatar-image"></div>
+              </li>
+              <li>
+                <div class="top-avatar-image"></div>
+              </li>
+              <li>
+                <div class="top-avatar-image"></div>
+              </li>
+              <li>
+                <div class="top-avatar-image"></div>
+              </li>
+              <li>
+                <div class="top-avatar-image"></div>
+              </li>
+            </ul>
+          </div>
+          <div class="subject-top-card">
+            <ul class="clearfix">
+              <li class="bg3">
+                <div class="card-avatar"></div>
+                <div class="user-name title3 word7 bold">
+                  床前明月光
+                </div>
+                <div class="card-ft clearfix word6 title4">
+                  <div class="like fl">
+                    <i></i>
+                    <span>162</span>
+                  </div>
+                  <div class="comment fl">
+                    <i></i>
+                    <span>121</span>
+                  </div>
+                  <div class="time fr">
+                    <span>5分钟前</span>
+                  </div>
+                </div>
+              </li>
+              <li class="bg3">
+                <div class="card-avatar"></div>
+                <div class="user-name title3 word7 bold">
+                  床前明月光
+                </div>
+                <div class="card-ft clearfix word6 title4">
+                  <div class="like fl">
+                    <i></i>
+                    <span>162</span>
+                  </div>
+                  <div class="comment fl">
+                    <i></i>
+                    <span>121</span>
+                  </div>
+                  <div class="time fr">
+                    <span>5分钟前</span>
+                  </div>
+                </div>
+              </li>
+              <li class="bg3"></li>
+              <li class="bg3"></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 <script>
+import swipe from './swipe'
 export default {
   data () {
     return {
     }
+  },
+  components:{
+    swipe
   }
 }
 </script>
@@ -43,4 +139,107 @@ export default {
         img 
           width 0.22rem
           height 0.22rem
+  .community-body
+    padding 0 0.15rem
+    .subject-banner
+      margin-top 0.15rem
+      .subject-info
+        position relative
+        padding-top 0.55rem
+        box-sizing border-box
+        padding-left 0.15rem
+        height 1.1rem
+        background-image url('../static/imgs/shequ_huati_bj.png')
+        background-size cover
+        .subject-info-title
+          font-size 0.2rem
+          padding-bottom 0.05rem
+        .more-subject
+          position absolute
+          right 0.1rem
+          bottom 0.12rem
+          line-height 0.24rem
+          width 0.64rem
+          text-align center
+          border-radius 2px
+          background-color #d2eaee
+      .subject-content
+        padding 0.1rem 0.15rem
+        line-height 0.24rem
+    .subject-top-user
+      border 1px solid #f4f4f4
+      padding 0.15rem
+      .subject-top-avatar
+        overflow-x auto
+        padding 0.13rem 0
+        ul
+          width 3.4rem
+          li
+            float left
+            padding 0 0.065rem
+            &:first-of-type
+              padding-left 0
+            &:last-of-type
+              padding-right 0
+            .top-avatar-image
+              height 0.34rem
+              width 0.34rem
+              background-image url('../static/imgs/shequ_zhuye_touxiang.png')
+              background-size cover
+      .subject-top-card
+        overflow-x auto
+        ul
+          width 10rem
+          li
+            float left
+            position relative
+            height 0.75rem
+            width 2rem
+            margin-right 0.15rem
+            &:last-of-type
+              margin-right 0
+            .card-avatar
+              position absolute
+              left 0.08rem
+              top 0.08rem
+              height 0.3rem
+              width 0.3rem
+              background-size cover
+              background-image url('../static/imgs/shequ_zhuye_liebiao_touxiang.png')
+            .user-name
+              position absolute
+              top 0.08rem
+              left 0.44rem
+              line-height 0.3rem
+            .card-ft
+              position absolute
+              line-height 0.2rem
+              width 100%
+              box-sizing border-box
+              bottom 0.08rem
+              left 0
+              padding 0 0.1 rem
+              .like
+                position relative
+                padding-left 0.23rem
+                padding-right 0.08rem
+                i
+                  position absolute
+                  top 0
+                  left 0
+                  width 0.2rem
+                  height 0.2rem
+                  background-image url(../static/imgs/shequ_zhuye_huizan.png)
+                  background-size cover
+              .comment
+                  position relative
+                  padding-left 0.23rem
+                i
+                  position absolute
+                  top 0
+                  left 0
+                  width 0.2rem
+                  height 0.2rem
+                  background-image url(../static/imgs/shequ_zhuye_huixiaoxi.png)
+                  background-size cover
 </style>
---
+++ src/modules/community/components/swipe.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="swipe">
+    <ul>
+      <li>
+        <div class="subject-img" :style="{'background-image':'url(https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=834281519,698021359&fm=58)'}"></div>
+        <div class="subject-info bg3">
+          <p class="subject-info-title title3 word7 bold">今日话题</p>
+          <p class="subject-info-count title4 word6">300000个主题</p>
+          <p class="subject-info-enter title4 word2">进入</p>
+        </div>
+        <i class="subject-avatar"></i>        
+      </li>
+      <li>
+        <div class="subject-img" :style="{'background-image':'url(https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=834281519,698021359&fm=58)'}"></div>
+        <div class="subject-info bg3">
+          <p class="subject-info-title title3 word7 bold">今日话题</p>
+          <p class="subject-info-count title4 word6">300000个主题</p>
+          <p class="subject-info-enter title4 word2">进入</p>
+        </div>
+        <i class="subject-avatar"></i>        
+      </li>
+      <li>
+        <div class="subject-img" :style="{'background-image':'url(https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=834281519,698021359&fm=58)'}"></div>
+        <div class="subject-info bg3">
+          <p class="subject-info-title title3 word7 bold">今日话题</p>
+          <p class="subject-info-count title4 word6">300000个主题</p>
+          <p class="subject-info-enter title4 word2">进入</p>
+        </div>
+        <i class="subject-avatar"></i>        
+      </li>
+      <li>
+        <div class="subject-img" :style="{'background-image':'url(https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=834281519,698021359&fm=58)'}"></div>
+        <div class="subject-info bg3">
+          <p class="subject-info-title title3 word7 bold">今日话题</p>
+          <p class="subject-info-count title4 word6">300000个主题</p>
+          <p class="subject-info-enter title4 word2">进入</p>
+        </div>
+        <i class="subject-avatar"></i>        
+      </li>
+      <li>
+        <div class="subject-img" :style="{'background-image':'url(https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=834281519,698021359&fm=58)'}"></div>
+        <div class="subject-info bg3">
+          <p class="subject-info-title title3 word7 bold">今日话题</p>
+          <p class="subject-info-count title4 word6">300000个主题</p>
+          <p class="subject-info-enter title4 word2">进入</p>
+        </div>
+        <i class="subject-avatar"></i>        
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+.swipe
+  margin-top 0.05rem
+  width 100%
+  overflow-x auto
+  ul
+    width 6.88rem
+    li 
+      float left
+      position relative
+      height 1.8rem
+      width 1.2rem
+      margin-right 0.22rem
+      &:last-of-type
+        margin-right 0
+      .subject-img
+        height 1rem
+        background-size cover
+        background-position center
+      .subject-info
+        height 0.8rem
+        padding-left 0.1rem
+        .subject-info-title
+          padding-top 0.22rem
+          padding-bottom 0.05rem
+        .subject-info-count
+          padding-bottom 0.1rem
+      .subject-avatar
+        position absolute
+        height 0.38rem
+        width 0.38rem
+        left 0.08rem
+        top 0.81rem
+        background-image url('../static/imgs/shequ_huati_touxiang.png')
+        background-size cover
+
+</style>
